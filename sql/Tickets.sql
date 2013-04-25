9de791389be229a147d7026d4a46592bfa5d4bbc
Pat Jan <pat_jan@hotmail.com>
2013-04-25 15:53:25 -0700

    Tickets.sql

---
+++ sql/Tickets.sql
@@ -0,0 +1,26 @@
+DROP   TABLE IF     EXISTS Tickets;
+CREATE TABLE IF NOT EXISTS Tickets
+( id				BIGINT				NOT NULL AUTO_INCREMENT
+, created_by		BIGINT				DEFAULT NULL
+, created_at		DATETIME			DEFAULT NULL
+, updated_by		BIGINT				DEFAULT NULL
+, updated_at		DATETIME			DEFAULT NULL
+, status			VARCHAR(32)			DEFAULT 'Active'
+
+, company_id		BIGINT				DEFAULT NULL
+, opened_by			BIGINT				DEFAULT NULL
+, opened_at			DATETIME			DEFAULT NULL
+, assigned_by		BIGINT				DEFAULT NULL
+, assigned_at		DATETIME			DEFAULT NULL
+, closed_by			BIGINT				DEFAULT NULL
+, closed_at			DATETIME			DEFAULT NULL
+, priority			VARCHAR(32)			DEFAULT NULL
+, description		TEXT				DEFAULT NULL
+, resolution		TEXT				DEFAULT NULL
+, attachments		TEXT				DEFAULT NULL
+
+, PRIMARY KEY(id)
+, KEY opened_by		(opened_by		)
+, KEY opened_at		(opened_at		)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1
+;
