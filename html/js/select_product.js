1bb7b7941b38fd91996b6dca77481e01231dda03
Pat Jan <pat_jan@hotmail.com>
2013-06-06 17:24:14 -0700

    product_name fix on ftp

--- html/js/select_product.js
+++ html/js/select_product.js
@@ -17,7 +17,7 @@ JKY.load_product = function() {
 		, filter	:  JKY.get_value('jky-search-filter')
 		, select	: 'All'
 		, display	: '10'
-		, order_by	: 'name'
+		, order_by	: 'product_name'
 		};
 	JKY.ajax(false, my_data, JKY.process_load_product_success);
 }
@@ -29,7 +29,7 @@ JKY.process_load_product_success = function(response) {
 	for(var i=0; i<my_rows.length; i++) {
 		var my_row = my_rows[i];
 		my_html += '<tr onclick="JKY.select_product(this, ' + my_row.id + ')">'
-				+  '<td class="jky-search-product-name"	>' + my_row.name			+ '</td>'
+				+  '<td class="jky-search-product-name"	>' + my_row.product_name			+ '</td>'
 				+  '<td class="jky-search-product-type"	>' + my_row.product_type	+ '</td>'
 				+  '<td class="jky-search-start-date"	>' + JKY.fix_ymd2dmy(my_row.start_date) + '</td>'
 				+  '</tr>'
@@ -46,7 +46,7 @@ JKY.add_new_product = function() {
 
 JKY.select_product = function(the_index, the_id) {
 	var my_name = $(the_index).find('.jky-search-product-name').html();
-//	JKY.display_message('id: ' + the_id + ', name: ' + my_name);
+//	JKY.display_message('id: ' + the_id + ',product name: ' + my_name);
 	JKY.set_value('jky-product-id', the_id);
 	JKY.set_value('jky-product', my_name);
 	JKY.hide_modal('jky-search-product');
