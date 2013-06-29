23e79e18719475c27fa97fc4df4c2e828e736c75
Pat Jan <pat_jan@hotmail.com>
2013-06-29 16:09:09 -0700

    Machine Cylinder

--- html/js/JKY.Cylinder.js
+++ html/js/JKY.Cylinder.js
@@ -15,39 +15,25 @@
  *		$(my_parent).find('.jky-cylinder-row-name').val(my_name);
  */
 JKY.Cylinder = function() {
+	var my_data			= null;
 	var my_the_id		= null;		//	external id that initiated the call
 	var my_order_by		= 'name';
-	var my_filter		= 'jky-Cylinder-filter';
-	var my_search_body	= 'jky-Cylinder-search-body';
-	var my_layer		= 'jky-Cylinder-search';
+	var my_filter		= 'jky-cylinder-filter';
+	var my_search_body	= 'jky-cylinder-search-body';
+	var my_layer		= 'jky-cylinder-search';
 
 	function my_display(the_id) {
 		my_the_id = the_id;
 		JKY.set_focus(my_filter);
-		my_load_data();
+		my_display_data()
 	}
 
-	function my_load_data() {
-		var my_data =
-			{ method	: 'get_index'
-			, table		: 'Cylinders'
-			, select	: 'All'
-			, filter	:  JKY.get_value(my_filter)
-			, display	: '10'
-			, order_by	:  my_order_by
-			};
-		JKY.ajax(false, my_data, my_load_data_success);
-	}
-
-	function my_load_data_success(response) {
-		var my_rows	= response.rows;
+	function my_display_data() {
 		var my_html = '';
-		for(var i=0; i<my_rows.length; i++) {
-			var my_row = my_rows[i];
-			my_html += '<tr onclick="JKY.Cylinder.click_row(this, ' + my_row.id + ')">'
-					+  '<td class="jky-Cylinder-search-name"			>' + my_row.name			+ '</td>'
-					+  '<td class="jky-Cylinder-search-group"			>' + my_row.Cylinder_group	+ '</td>'
-					+  '<td class="jky-Cylinder-search-composition"	>' + my_row.composition		+ '</td>'
+		for(var i=0, max=my_data.length; i<max; i++) {
+			var my_row = my_data[i];
+			my_html += '<tr onclick="JKY.Cylinder.click_row(this)">'
+					+  '<td class="jky-cylinder-search-name">' + my_row.name + '</td>'
 					+  '</tr>'
 					;
 		}
@@ -56,10 +42,9 @@ JKY.Cylinder = function() {
 	}
 
 
-	function my_click_row(the_index, the_id) {
-		var my_name = $(the_index).find('.jky-Cylinder-search-name').html();
+	function my_click_row(the_index) {
+		var my_name = $(the_index).find('.jky-cylinder-search-name').html();
 		var my_parent = $(my_the_id).parent();
-		$(my_parent).find('.jky-cylinder-row-id'  ).val(the_id );
 		$(my_parent).find('.jky-cylinder-row-name').val(my_name);
 		$(my_parent).find('.jky-cylinder-row-name').click();
 		JKY.hide_modal(my_layer);
@@ -69,10 +54,13 @@ JKY.Cylinder = function() {
 		JKY.display_message('add_new');
 	}
 
+	$(function() {
+		my_data = JKY.get_configs('Cylinders');
+	});
+
 	return {
 		  display		: function(the_id)				{		my_display(the_id);}
-		, load_data		: function()					{		my_load_data();}
-		, click_row		: function(the_index, the_id)	{		my_click_row(the_index, the_id);}
+		, click_row		: function(the_index)			{		my_click_row(the_index);}
 		, add_new		: function()					{		my_add_new();}
 	};
 }();
