6e8ada924408d2bdde7c1c3f54912bce0950913d
Pat Jan <pat_jan@hotmail.com>
2013-08-29 14:45:44 -0700

    pieces

---
+++ html/js/pieces.js
@@ -0,0 +1,112 @@
+"use strict";
+
+/**
+ * pieces.js
+ */
+
+/**
+ * start program
+ */
+JKY.start_program = function() {
+	JKY.App = JKY.Application;
+	JKY.App.set(
+		{ object_name	: 'JKY.App'
+		, program_name	: 'Pieces'
+		, table_name	: 'Pieces'
+		, specific		: ''
+		, select		: ''
+		, filter		: ''
+		, sort_by		: 'barcode'
+		, sort_seq		: 'DESC'
+		, focus			: 'jky-received-time'
+		, add_new		: 'display form'
+		});
+	JKY.App.init();
+};
+
+/**
+ *	set all events (run only once per load)
+ */
+JKY.set_all_events = function() {
+	$('#jky-tab-lines'		).click (function() {JKY.display_lines	();});
+	$('#jky-line-add-new'	).click (function() {JKY.insert_line	();});
+//	$('#jky-thread-filter'	).KeyUpDelay(JKY.Thread.load_data);
+};
+
+/**
+ *	set initial values (run only once per load)
+ */
+JKY.set_initial_values = function() {
+	JKY.set_side_active('jky-production-pieces');
+//	JKY.set_html('jky-app-select', JKY.set_configs('Product Types', JKY.App.get('select'), 'All'));
+//	JKY.set_html('jky-thread-name', JKY.set_options_array('', JKY.get_companies('is_supplier'), false));
+//	JKY.set_html('jky-payment-term', JKY.set_configs('Payment Terms', '', ''));
+//	JKY.set_html('jky-app-select-label', JKY.t('Type'));
+//	JKY.show('jky-app-select-line');
+};
+
+/**
+ *	set table row
+ */
+JKY.set_table_row = function(the_row) {
+	var my_html = ''
+		+  '<td class="jky-order-number"		>' +				 the_row.order_number			+ '</td>'
+		+  '<td class="jky-barcode"				>' +				 the_row.barcode				+ '</td>'
+		+  '<td class="jky-status"				>' +				 the_row.status					+ '</td>'
+		+  '<td class="jky-number-of-pieces"	>' +				 the_row.number_of_pieces		+ '</td>'
+		+  '<td class="jky-checkin-weight"		>' +				 the_row.checkin_weight			+ '</td>'
+		+  '<td class="jky-real-weight"			>' +				 the_row.real_weight			+ '</td>'
+		+  '<td class="jky-checkin-location"	>' +				 the_row.checkin_location		+ '</td>'
+		+  '<td class="jky-checkout-location"	>' + JKY.fix_null	(the_row.checkout_location	)	+ '</td>'
+		+  '<td class="jky-quality"				>' +				 the_row.quality				+ '</td>'
+		;
+	return my_html;
+};
+
+/**
+ *	set form row
+ */
+JKY.set_form_row = function(the_row) {
+	JKY.set_value	('jky-order-number'				, the_row.order_number			);
+	JKY.set_value	('jky-barcode'					, the_row.barcode				);
+	JKY.set_value	('jky-status'					, the_row.status				);
+	JKY.set_value	('jky-number-of-pieces'			, the_row.number_of_pieces		);
+	JKY.set_value	('jky-checkin-weight'			, the_row.checkin_weight		);
+	JKY.set_value	('jky-real-weight'				, the_row.real_weight			);
+	JKY.set_value	('jky-checkin-location'			, the_row.checkin_location		);
+	JKY.set_value	('jky-checkout-location'		, the_row.checkout_location		);
+	JKY.set_value	('jky-quality'					, the_row.quality				);
+//	JKY.display_lines();
+};
+
+/**
+ *	set add new row
+ */
+JKY.set_add_new_row = function() {
+	JKY.set_value	('jky-order-number'			, '');
+	JKY.set_value	('jky-barcode'				, '');
+	JKY.set_value	('jky-number-of-pieces'		, '0');
+	JKY.set_value	('jky-checkin-weight'		,  0);
+	JKY.set_value	('jky-real-weight'			,  0);
+	JKY.set_value	('jky-checkin-location'		, '');
+	JKY.set_value	('jky-checkout-location'	, '');
+	JKY.set_value	('jky-quality'				, '');
+}
+
+/**
+ *	get form set
+ */
+JKY.get_form_set = function() {
+//	var my_supplier_id = JKY.get_value('jky-thread-name');
+//	my_supplier_id = (my_supplier_id == '') ? 'null' : my_supplier_id;
+
+	var my_set = ''
+		+'	order_id='				+			  JKY.get_value('jky-order-number'			)
+		+', barcode=\''				+			  JKY.get_value('jky-barcode'				) + '\''
+		+', checkin_weight=  '		+			  JKY.get_value('jky-checkin-weight'		)
+		+', real_weight=  '			+			  JKY.get_value('jky-real-weight'			)
+		+', checkin_location=\''	+			  JKY.get_value('jky-checkin-location'		) + '\''
+		+', checkout_location=\''	+			  JKY.get_value('jky-checkout-location'		) + '\''
+		+', quality=\''				+			  JKY.get_value('jky-quality'				) + '\''
+	return my_set;
+};
