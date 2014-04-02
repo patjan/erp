d9eb1dc4e8f2533c55dd4021de5dbd6fb6ca07bf
shadobladez <joeljan92@Hotmail.com>
2014-04-01 18:08:32 -0700

    unknown

--- html/js/JKY.D3.js
+++ html/js/JKY.D3.js
@@ -52,6 +52,7 @@ JKY.D3 = function() {
 
 		var my_html = '';
 		if (my_args.graph_name == 'dual_bar')		{my_dual_bar(the_data);}
+        if (my_args.graph_name == 'donut')          {my_donut(the_data);}
 
 		return my_html;
 	};
@@ -188,7 +189,27 @@ JKY.D3 = function() {
 			;
     };
 
-	return {version	:	'1.0.0'
+    /**
+     *	draw donut
+     */
+    function my_donut(the_data) {
+        JKY.display_trace('mydonut');
+
+//		calculate the height based on number of json rows
+        var chart_width		= my_args.chart_width;
+        var chart_height	= the_data.length * row_height;
+
+// define svg dimension and bind to graph id
+        var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+    }
+
+        return {version	:	'1.0.0'
 		, setArgs	:	function(the_args)				{		my_setArgs(the_args)		;}
 		, getArgs	:	function()						{		my_getArgs()				;}
 		, set		:	function(the_arg, the_value)	{return my_set(the_arg, the_value)	;}
