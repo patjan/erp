e1c97f217fc6599a26dba51afca45802f80df564
shadobladez <joeljan92@Hotmail.com>
2014-05-06 15:30:35 -0700

    controllers

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -36,7 +36,7 @@ public function init() {
 		('host'		=> 'external-db.s122232.gridserver.com'
 		,'username'	=> 'db122232'
 		,'password'	=> 'brazil.18781'
-		,'dbname'	=> 'db122232_tecno'
+		,'dbname'	=> 'db122232_erp'
 		);
 
 	$db = Zend_Db::factory('PDO_MYSQL', $params);
